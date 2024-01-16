276163f95b72ff6bc2a8046adced832ad00cf20b
cmoulliard <cmoulliard@redhat.com>
2024-01-16 11:14:27 +0100

    Initial commit
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ packages/backend/src/plugins/scaffolder.ts
@@ -0,0 +1,35 @@
+import { CatalogClient } from '@backstage/catalog-client';
+import { Router } from 'express';
+import type { PluginEnvironment } from '../types';
+import { ScmIntegrations } from '@backstage/integration';
+import {createBuiltinActions, createRouter} from '@backstage/plugin-scaffolder-backend';
+import { cloneQuarkusQuickstart } from '@qshift/plugin-quarkus-backend';
+
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  const catalogClient = new CatalogClient({
+    discoveryApi: env.discovery,
+  });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [...builtInActions, cloneQuarkusQuickstart()];
+
+  return await createRouter({
+    actions,
+    logger: env.logger,
+    config: env.config,
+    database: env.database,
+    reader: env.reader,
+    catalogClient,
+    identity: env.identity,
+    permissions: env.permissions,
+  });
+}
