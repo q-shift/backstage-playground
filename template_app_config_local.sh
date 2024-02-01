5e3520d1c4e26c43588f585b3dc9a27c19693c20
A. Costa <antcosta@redhat.com>
2024-02-01 17:01:43 +0100

    feat: script option for template file location

--- template_app_config_local.sh
+++ template_app_config_local.sh
@@ -17,18 +17,25 @@ show_usage () {
     echo "For required parameters check the jinja2 template file being used."
     echo "The default template file is located at the ./manifest/app-config.local.yaml.j2" file.
     echo ""
+    echo "Optional parameters:"
+    echo ""
+    echo -e "--template_file <value>\t\tCustom location for the template file"
+    echo -e "--help\t\t\t\tShow this message"
+    echo ""
     exit 1
 }
 
+JINJA2_TEMPLATE_FILE="./manifest/app-config.local.yaml.j2"
 JINJA2_TEMPLATE_VARIABLES=""
 INVALID_SWITCH=""
 
 set +e
 while [ $# -gt 0 ]; do
-    #echo "$1"
+    # echo "$1"
     if [[ $1 == "--"* ]]; then
         case $1 in
             --help) show_usage; break 2 ;;
+            --template_file) JINJA2_TEMPLATE_FILE="${2}"; shift; shift ;; 
             *) show_usage "Invalid switch $1" ; break 2 ;;
         esac
     elif [[ $1 == "-"* ]]; then
@@ -44,13 +51,14 @@ set -e
 
 echo " "
 
+echo "Template file location: ${JINJA2_TEMPLATE_FILE}"
 echo "Templating app-config with the following parameters: "
 echo "  ${JINJA2_TEMPLATE_VARIABLES}"
 
 echo "Starting the template process..."
 
-#echo "jinja2 ${JINJA2_TEMPLATE_VARIABLES} ./manifest/app-config.local.yaml.j2 > app-config.local.yaml"
+#echo "jinja2 ${JINJA2_TEMPLATE_VARIABLES} ${JINJA2_TEMPLATE_FILE} > app-config.local.yaml"
 #jinja2 ${JINJA2_TEMPLATE_VARIABLES} ./manifest/app-config.local.yaml.j2
-jinja2 ${JINJA2_TEMPLATE_VARIABLES} ./manifest/app-config.local.yaml.j2 --strict > app-config.local.yaml
+jinja2 ${JINJA2_TEMPLATE_VARIABLES} ${JINJA2_TEMPLATE_FILE} --strict > app-config.local.yaml
 
 echo "Template process finished!"
