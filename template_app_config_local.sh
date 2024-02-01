250a0ef2d65f534251ccb5d8e047c3eb06604049
A. Costa <antcosta@redhat.com>
2024-02-01 12:06:38 +0100

    chore: added help to the script

--- template_app_config_local.sh
+++ template_app_config_local.sh
@@ -3,17 +3,35 @@
 #
 # Script for templating the jinja2 app-config.local.yaml file.
 #
+# It collectes all the parameters passed with the - switch into jinja2 
+#  key/value variables that are to be replaced inside the jinja2 template.
 
 set -o errexit
 
-JINJA2_TEMPLATE_VARIABLES=""
+show_usage () {
+    echo ""
+    echo >&2 "$@"
+    echo ""
+    echo "Script for templating the jinja2 app-config.yaml file."
+    echo ""
+    echo "For required parameters check the jinja2 template file being used."
+    echo "The default template file is located at the ./manifest/app-config.local.yaml.j2" file.
+    echo ""
+    exit 1
+}
 
-echo ""
+JINJA2_TEMPLATE_VARIABLES=""
+INVALID_SWITCH=""
 
 set +e
 while [ $# -gt 0 ]; do
     #echo "$1"
-    if [[ $1 == "-"* ]]; then
+    if [[ $1 == "--"* ]]; then
+        case $1 in
+            --help) show_usage; break 2 ;;
+            *) show_usage "Invalid switch $1" ; break 2 ;;
+        esac
+    elif [[ $1 == "-"* ]]; then
         param="${1/-/}";
         shift;
         value="${1}";
