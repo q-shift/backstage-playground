9e4ce23dc2edc05f791d60d8313316bc93f388d5
cmoulliard <cmoulliard@redhat.com>
2024-05-21 18:00:15 +0200

    Fix issue reported within #88 such as violation error. Switch to a more recent version of yarn, add signin page which is needed to use guest auth provider
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,7 +2,6 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 backend.add(import('@backstage/plugin-auth-backend'));
-// TODO: Check to add github auth provider
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
 backend.add(import('@backstage/plugin-app-backend/alpha'));
@@ -11,28 +10,32 @@ backend.add(
     import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
-// TODO: To be added in a separate PR: backend.add(import('@backstage/plugin-devtools-backend'));
-backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
 backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-explore/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
-backend.add(
-    import('@backstage/plugin-catalog-backend-module-backstage-openapi'),
-);
 backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.add(import('@backstage/plugin-notifications-backend'));
 
-// TODO: Section to be reviewed to add/remove plugins
-// //backend.add(import('@backstage/plugin-signals-backend'));
+backend.add(import('@backstage/plugin-permission-backend-module-allow-all-policy'));
 
 // Argocd stuffs
 backend.add(import('./plugins/argocd-backend/index'))
 backend.add(import('./plugins/argocd-actions/index'))
+
+// TODO: Section to be reviewed to add/remove plugins
+// //backend.add(import('@backstage/plugin-signals-backend'));
+// backend.add(import('@backstage/plugin-catalog-backend-module-backstage-openapi'));
+// backend.add(import('@backstage/plugin-search-backend-module-explore/alpha'));
+// backend.add(import('@backstage/plugin-permission-backend/alpha'));
+
+// TODO: To be added in a separate PR: backend.add(import('@backstage/plugin-devtools-backend'));
+backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+
+// TODO: Add the github auth provider as janus-idp is using it
 
 backend.start()
