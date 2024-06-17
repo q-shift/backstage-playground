229ad62d6d0eb0be76969c0963724cca6fd1987b
Charles Moulliard <cmoulliard@redhat.com>
2024-06-17 08:58:25 +0200

    Add gitea scaffold module. #157 (#158)
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -15,6 +15,7 @@ backend.add(import('@backstage/plugin-permission-backend/alpha'));
 backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-gitea'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 backend.add(import('@backstage/plugin-search-backend/alpha'));
